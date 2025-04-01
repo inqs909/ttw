50198a3b6dca6bb85c954ec5cdad36e0cbe3def0
Isaac Quintanilla Salinas <inqs.909@gmail.com>
2025-04-01 08:20:28 -0700

    Updated Files

---
+++ posts/wk_3_4_25/plot_code.R
@@ -0,0 +1,75 @@
+## Loading Libraries
+library(tidyverse)
+library(csucistats)
+library(ggridges)
+library(camcorder)
+
+
+## Loading Data
+pokemon_df <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-04-01/pokemon_df.csv')
+
+## Viewing Data
+glimpse(pokemon_df)
+
+## Names of variables
+names(pokemon_df)
+
+## Categorical Data
+cat_stats(pokemon_df$egg_group_1)
+cat_stats(pokemon_df$type_1)
+
+## Numerical Data
+num_stats(pokemon_df$base_experience)
+ggplot(pokemon_df, aes(base_experience)) + 
+  geom_density()
+
+## Recording Progress
+
+gg_record(
+  dir = file.path("posts", "wk_4_1_25"),
+  device = "png",
+  width = 6,
+  height = 6,
+  units = "in",
+  dpi = 300
+)
+
+## Plotting 
+
+ggplot(
+    pokemon_df, 
+    aes(x = base_experience, y = str_to_title(type_1), fill = factor(stat(quantile)))
+  ) +
+  stat_density_ridges(
+    geom = "density_ridges_gradient", calc_ecdf = TRUE,
+    quantiles = 4, quantile_lines = TRUE
+  ) +
+  scale_x_continuous(limits = c(-50, 475), expand = c(0, 0)) +
+  labs(
+    y = NULL,
+    x = "Base Experience",
+    fill = NULL,
+    title = "Pokémon Density Ridgeline Plot",
+    subtitle = "Distribution of Base Experience by Pokémon Type"
+  ) +
+  scale_fill_viridis_d(name = "Quartiles", option = "C") +
+  theme_bw()
+
+
+## Saving Plot
+ggsave(
+  filename = file.path("posts", "wk_4_1_25", paste0("final", ".png")),
+  height = 6,
+  width = 6,
+  units = "in",
+  dpi = 300
+)
+
+
+## Saving GIF
+gg_playback(
+  name = file.path("posts", "wk_4_1_25", paste0("final", ".gif")),
+  first_image_duration = 4,
+  last_image_duration = 20,
+  frame_duration = .25
+)
